7fd3bb60294693f641abe6728af1e1a67dea3fb4
lanqing2020 <313680654@qq.com>
2024-11-28 22:01:49 +0800

    实现每隔5s接受数据并重绘echarts

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -2,40 +2,46 @@
 import * as echarts from "echarts";
 import {onMounted, onUnmounted, reactive, ref} from "vue";
 import {echartsConst} from "@/constants/echarts";
-import {send} from "vite";
 
 const echartsTemperatureRef = ref(null);
-const initTemperatureEcharts = () => {
-  const temperatureCharts = echarts.init(echartsTemperatureRef.value);
-  const data = reactive([]);
+let echartsData = reactive([]);
+const count = ref(0);
+// [
+//   [0, 3, 15],
+//   [1, 3.6, 18],
+//   [2, 3.2, 16],
+//   [3, 4, 20],
+//   [4, 6, 30],
+//   [5, 5, 25],
+//   [6, 6.4, 32],
+//   [7, 5.8, 29],
+//   [8, 7.2, 36],
+//   [9, 8.4, 42],
+//   [10, 9.4, 47],
+//   [11, 8.4, 42],
+//   [12, 7.2, 36],
+//   [13, 6, 30],
+//   [14, 3.6, 18],
+//   [15, 8.4, 42],
+//   [16, 8.4, 42],
+//   [17, 8.4, 42],
+//   [18, 8.4, 42],
+//   [19, 8.4, 42],
+//   [20, 8.4, 42],
+//   [21, 8.4, 42],
+// ].map(function (item) {
+//   return [item[0], item[1], item[2]];
+// })
 
-  // [
-  //   [0, 3, 15],
-  //   [1, 3.6, 18],
-  //   [2, 3.2, 16],
-  //   [3, 4, 20],
-  //   [4, 6, 30],
-  //   [5, 5, 25],
-  //   [6, 6.4, 32],
-  //   [7, 5.8, 29],
-  //   [8, 7.2, 36],
-  //   [9, 8.4, 42],
-  //   [10, 9.4, 47],
-  //   [11, 8.4, 42],
-  //   [12, 7.2, 36],
-  //   [13, 6, 30],
-  //   [14, 3.6, 18],
-  //   [15, 8.4, 42],
-  //   [16, 8.4, 42],
-  //   [17, 8.4, 42],
-  //   [18, 8.4, 42],
-  //   [19, 8.4, 42],
-  //   [20, 8.4, 42],
-  //   [21, 8.4, 42],
-  // ].map(function (item) {
-  //   return [item[0], item[1], item[2]];
-  // })
-
+/**
+ * 构造data结构及数据
+ */
+const structureData = (data: number) => {
+  const item = [count.value, parseInt((data/5).toFixed(1)), data];
+  echartsData.push(item);
+  echartsData = echartsData.map(function (val) {
+    return [val[0], val[1], val[2]];
+  })
   const option = {
     title: {
       show: true,
@@ -93,20 +99,93 @@ const initTemperatureEcharts = () => {
         symbolSize: function (val: any) {
           return val[1] * 1.5;
         },
-        data: data,
+        data: echartsData,
         animationDelay: function (idx: any) {
           return idx * 100;
         }
       }
     ]
   };
-  temperatureCharts.setOption(option);
+  count.value += 1;
+  // console.log("echartsData===>", echartsData)
+  initTemperatureEcharts();
+}
+
+const initTemperatureEcharts = () => {
+  let myChart = echarts.getInstanceByDom(document.getElementById("my-chart"));
+  if (myChart == null) {
+    myChart = echarts.init(echartsTemperatureRef.value);
+  }
+  const option = {
+    title: {
+      show: true,
+      text: "设备温度折线图",
+      textStyle: {
+        fontSize: 16,
+        color: "#242633"
+      },
+      textAlign: "center",
+      textVerticalAlign: "center",
+      left: "45%",
+      top: "20px"
+    },
+    // legend未显示
+    legend: {
+      data: ["最近一小时"],
+      left: "right"
+    },
+    tooltip: {
+      position: 'top',
+      formatter: function (params: any) {
+        return ( echartsConst.xAxisHoursDefault[params.value[0]] + "点的温度是：" + params.value[2] + "°C" );
+      }
+    },
+    grid: {
+      left: 2,
+      bottom: 10,
+      right: 10,
+      containLabel: true
+    },
+    xAxis: {
+      name: "时间",
+      type: 'category',
+      boundaryGap: false,
+      splitLine: {
+        show: true
+      },
+      axisLine: {
+        show: false
+      },
+      data: echartsConst.xAxisHoursDefault
+    },
+    yAxis: {
+      name: "温度",
+      type: 'category',
+      data: echartsConst.yAxisTemperatureDefault,
+      axisLine: {
+        show: false
+      }
+    },
+    series: [
+      {
+        name: "最近一小时",
+        type: 'line',  // github是 scatter：https://echarts.apache.org/examples/zh/editor.html?c=scatter-punchCard
+        symbolSize: function (val: any) {
+          return val[1] * 1.5;
+        },
+        data: echartsData,
+        animationDelay: function (idx: any) {
+          return idx * 100;
+        }
+      }
+    ]
+  };
+  myChart.setOption(option);
 }
 
 const ws = ref(null);
 const status = ref("已断开");
 const message = ref([]);
-
 const connect = () => {
   ws.value = new WebSocket("ws://localhost:8083/websocket/111");
   ws.value.onopen = onOpen;
@@ -121,7 +200,7 @@ const onOpen = () => {
 
 const onMessage = (event: any) => {
   console.log("event===>", event)
-  // message.value.push(event.data);
+  structureData(parseInt(event.data));
 }
 
 const onError = (error: any) => {
@@ -168,7 +247,7 @@ onUnmounted(() => {
     <h2 class="title">设备温度</h2>
     <div class="body">
       <div class="echarts-wrap echarts-temperature">
-        <div class="canvas-wrap" ref="echartsTemperatureRef" style="width: 560px; height: 373px;" />
+        <div class="canvas-wrap" ref="echartsTemperatureRef" id="my-chart" style="width: 560px; height: 373px;" />
       </div>
     </div>
   </section>
