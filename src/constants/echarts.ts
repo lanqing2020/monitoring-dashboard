c832468207e38870929a363e9d3ddbfb93bf745d
lanqing2020 <313680654@qq.com>
2024-11-28 21:10:11 +0800

    初始化 websocket

---
+++ src/constants/echarts.ts
@@ -0,0 +1,7 @@
+const echartsConst = {
+    // echarts x轴坐标数据
+    xAxisHoursDefault: ['1min', '2min', '3min', '4min', '5min', '6min', '7min', '8min', '9min', '10min', '11min', '12min', '13min', '14min', '15min', '16min', '17min', '18min', '19min', '20min', '21min', '22min', '23min', '24min', '25min', '26min', '27min', '28min', '29min', '30min', '31min', '32min', '33min', '34min', '35min', '36min', '37min', '38min', '39min', '40min', '41min', '42min', '43min', '44min', '45min', '46min', '47min', '48min', '49min', '50min', '51min', '52min', '53min', '54min', '55min', '56min', '57min', '58min', '59min', '60min'],
+    yAxisTemperatureDefault: ["0", "5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "65", "60", "65", "70", "75", "80", "85", "90", "95", "100"],
+}
+
+export { echartsConst }
--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,30 +1,41 @@
 <script setup lang="ts">
 import * as echarts from "echarts";
-import {onMounted, ref} from "vue";
+import {onMounted, onUnmounted, reactive, ref} from "vue";
+import {echartsConst} from "@/constants/echarts";
+import {send} from "vite";
 
 const echartsTemperatureRef = ref(null);
 const initTemperatureEcharts = () => {
   const temperatureCharts = echarts.init(echartsTemperatureRef.value);
-  const hours = [
-    '5min', '10min', '15min', '20min', '25min', '30min', '35min', '40min', '45min', '50min', '55min', '60min'
-  ];
-  const temperatures = ["0", "5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "65", "60", "65", "70", "75", "80"];
-  const data = [
-    [0, 3, 15],
-    [1, 3.6, 18],
-    [2, 3.2, 16],
-    [3, 4, 20],
-    [4, 6, 30],
-    [5, 5, 25],
-    [6, 6.4, 32],
-    [7, 5.8, 29],
-    [8, 7.2, 36],
-    [9, 8.4, 42],
-    [10, 9.4, 47],
-    [11, 8.4, 42],
-  ].map(function (item) {
-    return [item[0], item[1], item[2]];
-  });
+  const data = reactive([]);
+
+  // [
+  //   [0, 3, 15],
+  //   [1, 3.6, 18],
+  //   [2, 3.2, 16],
+  //   [3, 4, 20],
+  //   [4, 6, 30],
+  //   [5, 5, 25],
+  //   [6, 6.4, 32],
+  //   [7, 5.8, 29],
+  //   [8, 7.2, 36],
+  //   [9, 8.4, 42],
+  //   [10, 9.4, 47],
+  //   [11, 8.4, 42],
+  //   [12, 7.2, 36],
+  //   [13, 6, 30],
+  //   [14, 3.6, 18],
+  //   [15, 8.4, 42],
+  //   [16, 8.4, 42],
+  //   [17, 8.4, 42],
+  //   [18, 8.4, 42],
+  //   [19, 8.4, 42],
+  //   [20, 8.4, 42],
+  //   [21, 8.4, 42],
+  // ].map(function (item) {
+  //   return [item[0], item[1], item[2]];
+  // })
+
   const option = {
     title: {
       show: true,
@@ -46,7 +57,7 @@ const initTemperatureEcharts = () => {
     tooltip: {
       position: 'top',
       formatter: function (params: any) {
-        return ( hours[params.value[0]] + "点的温度是：" + params.value[2] + "°C" );
+        return ( echartsConst.xAxisHoursDefault[params.value[0]] + "点的温度是：" + params.value[2] + "°C" );
       }
     },
     grid: {
@@ -65,12 +76,12 @@ const initTemperatureEcharts = () => {
       axisLine: {
         show: false
       },
-      data: hours
+      data: echartsConst.xAxisHoursDefault
     },
     yAxis: {
       name: "温度",
       type: 'category',
-      data: temperatures,
+      data: echartsConst.yAxisTemperatureDefault,
       axisLine: {
         show: false
       }
@@ -91,8 +102,56 @@ const initTemperatureEcharts = () => {
   };
   temperatureCharts.setOption(option);
 }
+
+const ws = ref(null);
+const status = ref("已断开");
+const message = ref([]);
+
+const connect = () => {
+  ws.value = new WebSocket("ws://localhost:8083/websocket/111");
+  ws.value.onopen = onOpen;
+  ws.value.onmessage = onMessage;
+  ws.value.onerror = onError;
+  ws.value.onclose = onClose;
+}
+
+const onOpen = () => {
+  status.value = "运行中";
+}
+
+const onMessage = (event: any) => {
+  console.log("event===>", event)
+  // message.value.push(event.data);
+}
+
+const onError = (error: any) => {
+  status.value = "Error";
+  console.error("WebSocket Error: ", error);
+}
+
+const onClose = () => {
+  status.value = "已断开";
+  ws.value = null;
+}
+
+const sendMessage = () => {
+  if (ws.value && ws.value.readyState === WebSocket.OPEN) {
+    ws.value.send("Your message here");
+  }
+}
+
+const disconnect = () => {
+  if (ws.value) {
+    ws.value.close();
+  }
+}
+
 onMounted(() => {
-  initTemperatureEcharts();
+  connect();
+})
+
+onUnmounted(() => {
+  disconnect();
 })
 
 </script>
@@ -101,7 +160,8 @@ onMounted(() => {
   <section class="state">
     <h2 class="title">设备状态</h2>
     <div class="body">
-      <div class="text">运行中</div>
+      <div class="text">{{ status }}</div>
+      <button @click="disconnect">断开</button>
     </div>
   </section>
   <section class="temperature sections">
